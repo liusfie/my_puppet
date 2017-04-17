c69e185ce7784a2314ab21aeb44853aa7c44916f
liusfie <zhenyuan90@163.com>
2017-04-17 22:36:12 +0800

    1.0

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,8 @@
 import 'nodes/*.pp'
 
+#monit
+import 'monit/classes/*.pp'
+
 node default {
   notify{
     "error! not match your node , this is default node":
--- modules/monit/manifests/init.pp
+++ modules/monit/manifests/init.pp
@@ -1,17 +1,107 @@
-class monit_memcached{
+class monit{
+package {
+	["monit"]:
+	ensure => installed;
+  }
+  
+  file { "monit_init":
+    name =>"/etc/init.d/monit",
+    mode =>755,
+    ensure =>present,
+    content =>template("monit/monit_init.erb"),
+  }
+  exec { "monit_graceful":
+    command => "/etc/init.d/monit restart",
+    path => ["/usr/bin", "/usr/sbin"],
+    user => "root",
+    refreshonly => true,
+    onlyif => ["/usr/local/monit/bin/monit -t"],
+  }
+	
+  define setmyconf ( $conf,$templatefile,$monit_ip,$ensure='present',$myport ) {
+	file { $conf:
+	mode=>700,
+	ensure =>present,
+	content => template($templatefile),
+#	require=>Package [monit],
+	}       
+  }
+  
+  define setconf ( $conf,$templatefile,$monit_ip,$ensure='present' ) {
+	file { $conf:
+	ensure => present,
+	content => template($templatefile),
+	}       
+  }
 
-  define setmemconf ( $memport,$memip ) {
-		file { "/usr/local/monit/etc/monit.d/memcached.cfg":
-		ensure => present,
-		content => template("monit/memcached.cfg.erb"),
-		}  
-		
-    exec { "memcached_monit":
-        command => "systemctl restart monit",
-        path => ["/usr/bin", "/usr/sbin"],
-        user => "root",
-        subscribe => File["/usr/local/monit/etc/monit.d/memcached.cfg"],
-        refreshonly => true,
-    }	
-  }		
+  define setconffile ( $conf,$conffile,$ensure='present' ) {
+	file {$conf:
+	path => $conf,
+	ensure => present,
+	owner     => root,
+	group     => root,
+	mode      => 700,
+	source => "puppet://puppetmaster.light.soufun.com/modules/monit/$conffile",
+
+		}
+	}
+	
+  define baseconf ( $conf,$templatefile,$ensure='present' ) {
+		file { $conf:
+		content => template($templatefile),
+#		require => Package [monit],
+		mode =>700,
+
+		}       
+	}
+   baseconf { "basemonit":
+			   conf => "/usr/local/monit/etc/monit.d/base.cfg",
+				 templatefile => "monit/base.cfg.erb",
+				 notify => Exec ["monit_graceful"],
+				 }
+	
+  baseconf { "monitrc":
+     conf => "/usr/local/monit/etc/monitrc",
+     templatefile => "monit/monitrc.erb",
+     notify => Exec ["monit_graceful"],
+  }
+
+ baseconf { "sys_monit":
+			   conf => "/usr/local/monit/etc/monit.d/sys_resource.cfg",
+				 templatefile => "monit/sys_resource.cfg.erb",
+				 notify => Exec ["monit_graceful"],
+				 } 
+				 
+
+ baseconf { "rsyncd_init":
+			   conf => "/etc/init.d/rsyncd",
+				 templatefile => "monit/rsyncd.erb",
+				 } 	
+				 			 
+  baseconf { "funcd_init":
+    conf => "/etc/init.d/funcd",
+    templatefile => "monit/funcd.erb",
+  }
+#  case $operatingsystem {
+#    redhat: {
+#      baseconf { "sshd_init":
+#        conf => "/etc/init.d/sshd",
+#        templatefile => "monit/sshd.erb",
+#      }
+#      baseconf { "snmpd_init":
+#        conf => "/etc/init.d/snmpd",
+#        templatefile => "monit/snmpd.erb",
+#      }
+#    }
+#    centos: {
+#      baseconf { "sshd_init":
+#        conf => "/etc/init.d/sshd",
+#        templatefile => "monit/centos_sshd.erb",
+#      }
+#      baseconf { "snmpd_init":
+#        conf => "/etc/init.d/snmpd",
+#        templatefile => "monit/centos_snmpd.erb",
+#      }
+#    }
+#  }
 }
